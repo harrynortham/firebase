036f0fe121c560af5d9f5eeb4d3e8543e34f3cea
Harry Northam <40062105+harrynortham@users.noreply.github.com>
2023-03-06 16:42:49 +0100

    Protected login/register routes from already authenticated users

--- src/App.js
+++ src/App.js
@@ -8,29 +8,31 @@ import ResetPassword from "./pages/ResetPassword";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import AnonymousRoute from "./routes/AnonymousRoute";
 
 // NEED TO SET CORRECT FIREBASE PERMISSIONS, TO BLOCK ACCESS TO USERS, LEARN
 
 const App = () => {
   return (
-    // Wrap routes in our Authentication context
+    // TODO Add a route for Admin users
+    // Wrap routes in our Authentication context provider
     // Wrap routes in our Layout containing the Header and Footer
+    // Create protected route to stop authenticated users accessing register/login/reset password+ https://stackoverflow.com/questions/74045405/how-to-disable-user-to-access-login-register-page-once-they-login
 
     <UserAuthContextProvider>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
+
+          <Route element={<AnonymousRoute />}>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
--- src/routes/AnonymousRoute.js
+++ src/routes/AnonymousRoute.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useUserAuth } from "../context/userAuthContext";
-const ProtectedRoute = () => {
+const AnonymousRoute = () => {
   const { user } = useUserAuth();
 
   //console.log("Check user in Private: ", user);
-  if (!user) {
-    return <Navigate to="/login" />;
+  if (user) {
+    return <Navigate to="/dashboard" />;
   }
+
   // render child elements with outlet https://reactrouter.com/en/main/components/outlet
   return <Outlet />;
 };
 
-export default ProtectedRoute;
+export default AnonymousRoute;
